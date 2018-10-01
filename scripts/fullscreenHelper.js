bfe61afa669c02ad76caa348bf1c1db92508a635
maxwellito <duluc.maxime@gmail.com>
2018-10-01 23:48:49 +0100

    Add intro details

---
+++ scripts/fullscreenHelper.js
@@ -0,0 +1,58 @@
+/**
+ * Fullscreen polyfills
+ */
+
+var fullscreenHelper = {
+
+  /**
+   * Checks if the browser is already in fullscreen mode
+   */
+  isFullscreen: function () {
+    return  (document.fullScreenElement && document.fullScreenElement !== null) ||
+            (document.mozFullScreen || document.webkitIsFullScreen);
+  },
+  
+  /**
+   * Enter in fullscreen mode
+   */
+  enterFullscreen: function () {
+    var docElm = document.documentElement;
+  
+    if (docElm.requestFullscreen) {
+      docElm.requestFullscreen();
+    }
+    else if (docElm.mozRequestFullScreen) {
+      docElm.mozRequestFullScreen();
+    }
+    else if (docElm.webkitRequestFullScreen) {
+      docElm.webkitRequestFullScreen();
+    }
+  },
+  
+  /**
+   * Exit the fullscreen mode
+   */
+  exitFullscreen: function () {
+    if (document.documentElement.exitFullscreen) {
+      document.documentElement.exitFullscreen();
+    }
+    else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    }
+    else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  },
+
+  /**
+   * Toggle the fullscreen mode
+   */
+  toggle: function () {
+    if (!fullscreenHelper.isFullscreen()) {
+      fullscreenHelper.enterFullscreen();
+    }
+    else {
+      fullscreenHelper.exitFullscreen();
+    }
+  }
+}
